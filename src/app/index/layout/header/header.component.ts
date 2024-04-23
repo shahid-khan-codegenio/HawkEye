81c1c257a259249bb55d76c8d8f98c0a9fc6ea86
yasirsaleemvws <147795322+yasirsaleemvws@users.noreply.github.com>
2024-04-23 11:06:20 +0500

    convert to scss

--- src/app/index/layout/header/header.component.ts
+++ src/app/index/layout/header/header.component.ts
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  navbarOpen: boolean = false;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  toggleNavbar() {
+    this.navbarOpen = !this.navbarOpen
   }
 
 }
