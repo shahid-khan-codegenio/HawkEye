e46dc90029c58fcbfa64e32344e18026bd8d49b4
yasirsaleemvws <147795322+yasirsaleemvws@users.noreply.github.com>
2024-04-24 11:38:56 +0500

    update testimonials

--- src/app/index/about/testimonials/testimonials.component.ts
+++ src/app/index/about/testimonials/testimonials.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./testimonials.component.scss']
 })
 export class TestimonialsComponent implements OnInit {
+  count = [1, 2, 3, 4]
   constructor() { }
 
   ngOnInit() {
