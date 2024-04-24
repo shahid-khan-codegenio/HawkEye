dbb21462f5d9ba56bd5c4bfd1eaa6634cf5dd5b6
yasirsaleemvws <147795322+yasirsaleemvws@users.noreply.github.com>
2024-04-24 12:27:28 +0500

    udpate routerlink

--- src/app/index/layout/layout.module.ts
+++ src/app/index/layout/layout.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
-  declarations: [HeaderComponent,FooterComponent],
-  exports: [HeaderComponent,FooterComponent]
+  declarations: [HeaderComponent, FooterComponent],
+  exports: [HeaderComponent, FooterComponent]
 })
 export class LayoutModule { }
