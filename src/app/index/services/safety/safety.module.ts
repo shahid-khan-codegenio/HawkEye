e1963498cbf2d56490ace29c8725a48ccb7fd264
yasirsaleemvws <147795322+yasirsaleemvws@users.noreply.github.com>
2024-04-28 23:03:23 +0500

    updated

--- src/app/index/services/safety/safety.module.ts
+++ src/app/index/services/safety/safety.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SafetyComponent } from './safety.component';
 import { RouterModule, Routes } from '@angular/router';
+import { SharedModule } from '../../shared/shared.module';
 
 const routes: Routes = [
   { path: '', component: SafetyComponent }
@@ -9,7 +10,8 @@ const routes: Routes = [
 @NgModule({
   imports: [
     CommonModule,
-    RouterModule.forChild(routes)
+    RouterModule.forChild(routes),
+    SharedModule
   ],
   declarations: [SafetyComponent]
 })
