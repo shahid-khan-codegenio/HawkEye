7ba0f8ff37a4fbee024975e9e22f64aedd934245
yasirsaleemvws <147795322+yasirsaleemvws@users.noreply.github.com>
2024-04-29 14:53:13 +0500

    udpated

--- src/app/index/about/earn-reward/earn-reward.module.ts
+++ src/app/index/about/earn-reward/earn-reward.module.ts
@@ -2,17 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { EarnRewardComponent } from './earn-reward.component';
 import { Routes, RouterModule } from '@angular/router';
+import { SharedModule } from '../../shared/shared.module';
 
 const routes: Routes = [
   {
-    path: '', 
+    path: '',
     component: EarnRewardComponent
   }
 ]
 @NgModule({
   imports: [
     CommonModule,
-    RouterModule.forChild(routes)
+    RouterModule.forChild(routes),
+    SharedModule
 
   ],
   declarations: [EarnRewardComponent]
