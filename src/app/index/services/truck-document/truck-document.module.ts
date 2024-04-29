0a13c6946066fe7cbfbc4d662acdc92e93c791bc
yasirsaleemvws <147795322+yasirsaleemvws@users.noreply.github.com>
2024-04-29 14:35:03 +0500

    updated

--- src/app/index/services/truck-document/truck-document.module.ts
+++ src/app/index/services/truck-document/truck-document.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TruckDocumentComponent } from './truck-document.component';
 import { Routes, RouterModule } from '@angular/router';
+import { SharedModule } from '../../shared/shared.module';
 
 const routes: Routes = [
   { path: '', component: TruckDocumentComponent }
@@ -10,7 +11,8 @@ const routes: Routes = [
 @NgModule({
   imports: [
     CommonModule,
-    RouterModule.forChild(routes)
+    RouterModule.forChild(routes),
+    SharedModule
   ],
   declarations: [TruckDocumentComponent]
 })
