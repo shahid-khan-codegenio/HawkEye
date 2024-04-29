9261b09663997954e7a0ebbdb5c5ce29fd9b44c7
yasirsaleemvws <147795322+yasirsaleemvws@users.noreply.github.com>
2024-04-29 14:30:01 +0500

    udpated

--- src/app/index/services/trucking-invoice/trucking-invoice.module.ts
+++ src/app/index/services/trucking-invoice/trucking-invoice.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TruckingInvoiceComponent } from './trucking-invoice.component';
 import { Routes, RouterModule } from '@angular/router';
+import { SharedModule } from '../../shared/shared.module';
 
 const routes: Routes = [
   { path: '', component: TruckingInvoiceComponent }
@@ -10,7 +11,8 @@ const routes: Routes = [
 @NgModule({
   imports: [
     CommonModule,
-    RouterModule.forChild(routes)
+    RouterModule.forChild(routes),
+    SharedModule
   ],
   declarations: [TruckingInvoiceComponent]
 })
