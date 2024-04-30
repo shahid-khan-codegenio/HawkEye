49c49d7b6c779334e342cf5052d79216ec5668cc
yasirsaleemvws <147795322+yasirsaleemvws@users.noreply.github.com>
2024-04-30 10:48:13 +0500

    faq updated

--- src/app/index/FAQ/FAQ.component.ts
+++ src/app/index/FAQ/FAQ.component.ts
@@ -2,14 +2,39 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-FAQ',
-  templateUrl: './FAQ.component.html',
-  styleUrls: ['./FAQ.component.scss']
+  templateUrl: './FAQ.component.html'
 })
 export class FAQComponent implements OnInit {
-
+  faq1 = false;
+  faq2 = false;
+  faq3 = false;
+  faq4 = false;
+  faq5 = false;
+  faq6 = false;
+  faq7 = false;
+  faq8 = false;
   constructor() { }
 
   ngOnInit() {
   }
 
+  toggleCollapse(section: string) {
+    if (section == 'faq1') {
+      this.faq1 = !this.faq1
+    } else if (section == 'faq2') {
+      this.faq2 = !this.faq2
+    } else if (section == 'faq3') {
+      this.faq3 = !this.faq3
+    } else if (section == 'faq4') {
+      this.faq4 = !this.faq4
+    } else if (section == 'faq5') {
+      this.faq5 = !this.faq5
+    } else if (section == 'faq6') {
+      this.faq6 = !this.faq6
+    } else if (section == 'faq7') {
+      this.faq7 = !this.faq7
+    } else if (section == 'faq8') {
+      this.faq8 = !this.faq8
+    }
+  }
 }
