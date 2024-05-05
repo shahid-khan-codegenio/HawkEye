359a3b8beddb765f118b59fa37860f36003b59f7
yasirsaleemvws <147795322+yasirsaleemvws@users.noreply.github.com>
2024-05-06 00:51:07 +0500

    form integration

--- src/app/index/home/home.module.ts
+++ src/app/index/home/home.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { RouterModule, Routes } from '@angular/router';
+import { SharedModule } from '../shared/shared.module';
 
 const routes: Routes = [
   { path: '', component: HomeComponent }
@@ -9,6 +10,7 @@ const routes: Routes = [
 @NgModule({
   imports: [
     CommonModule,
+    SharedModule,
     RouterModule.forChild(routes)
   ],
   declarations: [HomeComponent]
